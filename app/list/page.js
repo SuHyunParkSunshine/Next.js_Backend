b1a58ca705a8e0e7c61a5d44e0ece6bd3e94f6da
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-12-08 17:58:58 +0900

    refactor : 글 목록 조회 map사용 코드 변경

--- app/list/page.js
+++ app/list/page.js
@@ -7,18 +7,16 @@ export default async function List() {
 
     return (
         <div className="list-bg">
-            <div className="list-item">
-                <h4>{result[0].title}</h4>
-                <p>{result[0].content}</p>
-            </div>
-            <div className="list-item">
-                <h4>{result[1].title}</h4>
-                <p>{result[1].content}</p>
-            </div>
-            <div className="list-item">
-            <h4>{result[2].title}</h4>
-                <p>{result[2].content}</p>
-            </div>
+            {
+                result.map((item) => {
+                    return (
+                        <div className="list-item">
+                            <h4>{item.title}</h4>
+                            <p>{item.content}</p>
+                        </div>
+                    )
+                })
+            }           
         </div>
     )
 }
